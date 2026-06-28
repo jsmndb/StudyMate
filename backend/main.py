8b9ed08213341b56530cba5fbe11d64dc8d52bca
jsmndb <jasmindbernal@gmail.com>
2026-06-28 18:28:43 +0800

    Connected React register page to FastAPI backend

--- backend/main.py
+++ backend/main.py
@@ -1,19 +1,33 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
+
 from routes.auth import router as auth_router
 from auth_middleware import verify_token
 
 app = FastAPI()
 
+# Allow React frontend to access FastAPI
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=[
+        "http://localhost:5173",
+    ],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 app.include_router(auth_router)
+
 
 @app.get("/")
 def home():
     return {"message": "StudyMate API is running"}
 
+
 @app.get("/profile")
 def profile(payload=Depends(verify_token)):
     return {
         "message": "Welcome back!",
-        "user_id": payload["user_id"]
+        "user_id": payload["user_id"],
     }
-
--- frontend/src/pages/RegisterPage.jsx
+++ frontend/src/pages/RegisterPage.jsx
@@ -1,5 +1,70 @@
-function RegisterdPage() {
-  return <h1>RegisterdPage</h1>;
+import { useState } from "react";
+import api from "../services/api";
+
+function RegisterPage() {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleRegister = async (e) => {
+    e.preventDefault();
+
+    try {
+      const response = await api.post("/register", {
+        name,
+        email,
+        password,
+      });
+
+      alert(response.data.message);
+
+      setName("");
+      setEmail("");
+      setPassword("");
+    } catch (error) {
+      alert("Registration failed");
+      console.error(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Register</h1>
+
+      <form onSubmit={handleRegister}>
+        <input
+          type="text"
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+
+        <br /><br />
+
+        <input
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+        />
+
+        <br /><br />
+
+        <button type="submit">
+          Register
+        </button>
+      </form>
+    </div>
+  );
 }
 
-export default RegisterdPage;
+export default RegisterPage;
